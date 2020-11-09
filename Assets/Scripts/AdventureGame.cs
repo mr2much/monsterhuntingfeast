02e09da00cf0f62fa9b4ebedc62d136b459b3c31
Anubis Lockward <anubis.lockward@gmail.com>
2020-11-09 19:47:37 -0400

    Initial commit

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    private State state;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.state = this.startingState;
+        UpdateStateStoryText();
+    }
+
+    void UpdateStateStoryText()
+    {        
+        textComponent.text = state.GetStateStoryText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageNextStates();
+    }
+
+    private void ManageNextStates()
+    {
+        var nextStates = state.GetNextStoryStates();
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Quitting!");
+            Application.Quit();
+        } else
+        {
+            for (var i = 0; i < nextStates.Length; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i) ||
+                  (Input.GetKeyDown(KeyCode.Keypad1 + i)))
+                {
+                    state = nextStates[i];
+                    UpdateStateStoryText();
+                    break;
+                }
+            }
+        }        
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{    
+    [TextArea(10,14)][SerializeField] string stateStoryText;
+    [SerializeField] State[] nextStoryStates;    
+
+    public string GetStateStoryText()
+    {
+        return this.stateStoryText;
+    }
+
+    public State[] GetNextStoryStates()
+    {
+        return this.nextStoryStates;
+    }
+}
