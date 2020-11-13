125daa32ddef3d4f478217979ce2444120849169
Anubis Lockward <anubis.lockward@gmail.com>
2020-11-12 23:33:42 -0400

    Started implementing triggerable choices
    Started modified the way options are currently handled to support Choices that affect other choices, like activating or deactivating them.

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 [Serializable]
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
-{    
-    [TextArea(10,14)][SerializeField] string stateStoryText;
-    [SerializeField] State[] nextStoryStates;    
-
+{
+    [TextArea(10, 14)] [SerializeField] string stateStoryText;
+    [SerializeField] List<String> optionsText;
+    [SerializeField] State[] nextStoryStates;
+    [SerializeField] bool active = true;
+    
     public string GetStateStoryText()
     {
         return this.stateStoryText;
     }
 
+    public List<String> GetOptionsText()
+    {
+        return optionsText;
+    }
+
     public State[] GetNextStoryStates()
     {
         return this.nextStoryStates;
+    }    
+
+    public void SetActive(bool newValue)
+    {
+        active = newValue;
+    }
+
+    public bool IsActive()
+    {
+        return active;
     }
 }
---
+++ Assets/Scripts/TriggeringState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(menuName ="Triggering State")]
+public class TriggeringState : State
+{
+    [SerializeField] State connectedEvent;
+
+    public void AffectConnectedEvent(bool newState)
+    {
+        connectedEvent.SetActive(newState);
+    }
+}
